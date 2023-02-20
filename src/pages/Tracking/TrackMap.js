5313cd022a41614ed0aaf0dbf64e6554e9c1e562
ravisachapara09 <ravisachapara.tst@gmail.com>
2023-02-20 10:51:28 +0530

    Update : MapRoute

--- src/pages/Tracking/TrackMap.js
+++ src/pages/Tracking/TrackMap.js
@@ -3,86 +3,55 @@ import PropTypes from "prop-types"
 import { GoogleApiWrapper, InfoWindow, Map, Marker, Polyline } from "google-maps-react"
 import { Col, Row } from "reactstrap"
 import { connect } from "react-redux"
-import CONSTANT from "../Utility/constnt"
 import useHttp from "../../components/Hook/Use-http"
 import axios from "axios"
 
 import defaultImage from "../../assets/images/UserImage.jpg";
 import MapTruck from '../../assets/images/MapTruck.png'
-// import './css/style.css'
+import Slider from "react-rangeslider"
+import "react-rangeslider/lib/index.css"
 
 const LoadingContainer = () => <div>Loading...</div>
 
 const TrackMap = (props) => {
+    const { data } = props
     const API_CALL = useHttp();
-    const [mapDataLive, setMapDataLive] = useState([])
     const [tripData, setTripData] = useState([])
-    console.log('TrackMap',props);
-    const mapData = [
-        {
-            position: { lat: 13.693, lng: 79.5946 },
-            id: 1,
-        },
-        {
-            position: { lat: 12.9716, lng: 77.5946 },
-        },
-        {
-            position: { lat: 8.5199, lng: 76.9413 },
-        },
-        {
-            position: { lat: 22.7196, lng: 75.8577 },
-        },
-        {
-            position: { lat: 18.5204, lng: 73.8567 },
-        },
-        {
-            position: { lat: 13.1984, lng: 78.4828 },
-        },
-    ]
+    const [mapDataLive, setMapDataLive] = useState([])
+    const [startEndLocation, setStartEndLocation] = useState([])
+    const [tripStep, setTripStep] = useState(0)
 
     useEffect(() => {
         (async () => {
-            API_CALL.sendRequest(CONSTANT.API.getMapLocation, MapLocationHandler);
-            API_CALL.sendRequest(CONSTANT.API.getAllTrip, getAllHandler);
+            setTripStep(0)
+            const URL = {
+                endpoint: `/location/${data.id}`,
+                type: "GET",
+            }
+            API_CALL.sendRequest(URL, MapLocationHandler);
         })();
-    }, []);
+    }, [data]);
 
     const MapLocationHandler = (res) => {
         const location = res?.data.map((data) => {
             if (data?.latitude && data?.longtitude) {
-                return { lat: data?.latitude, lng: data?.longtitude }
+                return {
+                    loc: { lat: data?.latitude, lng: data?.longtitude },
+                    time: data?.updateLocationTime,
+                    address: data?.detailedAddress
+                }
             }
         })
-        const data = location.filter((data) => data !== undefined)
-        setMapDataLive( location.filter((data) => data !== undefined) )
+        const mapRoute = location.filter((data) => data?.loc !== undefined)
+        setTripData(mapRoute)
+        if (mapRoute.length > 0) {
+            setStartEndLocation([mapRoute[0].loc, mapRoute[mapRoute.length - 1].loc]);
+        }
+        else {
+            setStartEndLocation([])
+        }
+        setMapDataLive(mapRoute.map((e) => e.loc))
     };
-
-    const getAllHandler = (data) => {
-        setTripData({
-            position: null,
-            id: data.id,
-            value: data.id,
-            adddatas: data?.sourceLocation,
-            plant: `${data?.plant?.unitName} ${data?.plant?.location}`,
-            vehicle: data?.vehicle,
-            driver: data?.driver,
-            startDate: data?.startDateAndTime,
-            completedDateAndTime: data?.targetedDateAndTime,
-            sourceLocation: data?.sourceLocation,
-            destinationLocation: data?.destinationLocation,
-        })
-    }
-
-    useEffect(() => {
-        axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=21.1702401,72.83106070000001&destination=19.0759837,72.8776559&key=AIzaSyAIh5rjUYY8SoLb14LUnxrbhD2XnRsF_78')
-            .then((e) => {
-                console.log('success', e)
-            })
-            .catch((e) => {
-                console.log('catch', e)
-            })
-    }, [])
-
 
     return (
         <div>
@@ -91,30 +60,39 @@ const TrackMap = (props) => {
                     <div
                         id="gmaps-markers"
                         className="gmapsCoustem"
-                        style={{ position: "relative", height: "84vh" }}
+                        style={{ position: "relative", height: "50vh" }}
                     >
                         <Map
                             google={props.google}
-                            style={{ width: "100%", height: "86vh", borderRadius: '10px' }}
-                            zoom={6}
-                            center={{ lat: 13.693, lng: 79.5946 }}
+                            style={{ width: "100%", height: "50vh", borderRadius: '10px' }}
+                            zoom={8}
+                            center={mapDataLive[0]}
                             fullscreenControl={false}
 
                         >
-                            {mapData.map((e) => {
+                            {startEndLocation.map((e) => {
                                 return (
                                     <Marker
                                         title={"The marker`s title will appear as a tooltip."}
                                         name={"SOMA"}
-                                        position={e.position}
-                                        // icon={{
-                                        //     url: MapTruck,
-                                        //     fillColor: '#EB00FF',
-                                        //     scaledSize: new google.maps.Size(60, 60),
-                                        // }}
+                                        position={e}
                                     />
                                 )
                             })
+                            }
+
+                            {
+                                tripStep > 0 &&
+                                <Marker
+                                    title={"The marker`s title will appear as a tooltip."}
+                                    name={"SOMA"}
+                                    position={mapDataLive[tripStep - 1]}
+                                    icon={{
+                                        url: MapTruck,
+                                        fillColor: '#EB00FF',
+                                        // scaledSize: new google.maps.Size(60, 60),
+                                    }}
+                                />
                             }
                             <Polyline
                                 path={mapDataLive}
@@ -124,15 +102,26 @@ const TrackMap = (props) => {
                                 strokeOpacity={0.8}
                                 strokeWeight={4}
                             />
-
-
-
                             <InfoWindow>
                                 <div>
                                     <h1>Surat</h1>
                                 </div>
                             </InfoWindow>
                         </Map>
+                        <Col md={3} className='MapRangeSlider'>
+                            <div className="p-3">
+                                <Slider
+                                    value={tripStep}
+                                    min={1}
+                                    max={tripData.length}
+                                    // labels={tripData.map((e) => e.time)}
+                                    orientation="horizontal"
+                                    onChange={value => {
+                                        setTripStep(value)
+                                    }}
+                                />
+                            </div>
+                        </Col>
                     </div>
                 </Col>
             </Row>
